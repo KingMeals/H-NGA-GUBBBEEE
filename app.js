f8e20d35ce62862ba39ed5d5f25ec09691579cb7
KingMeals <71371924+KingMeals@users.noreply.github.com>
2021-04-14 11:06:22 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+let gissade = '';
+let maxFel = 6;
+let startFel = 0;
+let alfabetetArray = new Array(29);
+
+function start() {
+
+};
+
+let ordList = [
+
+]
+
+class knapp {
+    constructor(onclick, element, classAttribute, letter)
+    {
+        this.onclick = onclick;
+        this.element = element;
+        this.classAttribute = classAttribute;
+    }
+}
+
+for (let i = 0; i < 29; i++) 
+{
+ alfabetetArray = new knapp(document.createAttribute("onclick"),
+                            document.createElement("div"),
+                            document.createElement(),
+                            string.fromCarCode(97 + i),
+                            );
+}
+
+for (let i = 0; i < 29; i++) {
+    alphabetAtt = new knapp(document.createAttribute("onclick"))
+}
+
+
+
+
